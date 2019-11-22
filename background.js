5d26f573d62bf1ba77daf9ed000d07619aaaff43
anishbadri@gmail.com <anishbadri@gmail.com>
2019-11-22 20:09:36 +0530

    impulse options page with test Embed

--- background.js
+++ background.js
@@ -1,3 +1,45 @@
 console.log("Hello");
 
-Chrome.contextMenu.create({});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+    if(isUrlActiveYTVideo(changeInfo.url))
+    {
+        videoId = getVideoIdFromUrl(changeInfo.url);
+        console.log(tabId);
+
+        // if(videoId){
+        //     //save video ID
+        //     incrementVideoCount(new Date().toLocaleDateString()).then((result)=> {updateBadge()})
+        // } 
+        addVideoIdInStorage(videoId);
+        // videoCount++;
+        // console.log("Url changed to ", changeInfo.url);
+        chrome.tabs.update(tabId, {url: chrome.runtime.getURL('index.html')});
+    }
+})
+
+function isUrlActiveYTVideo(url){
+    //watch regex
+    const youTubeWatchRegex = new RegExp("/*youtube.com/watch*");
+    if(youTubeWatchRegex.exec(url)){
+        return true;
+    }
+    return false;
+}
+
+function getVideoIdFromUrl(url){
+    //works only for a striaght forward Url 
+    const regex = /https\:\/\/www\.youtube\.com\/watch\?v=([\w-]{11})/;
+    return url.match(regex) ? url.match(regex)[1]: null;
+}
+
+function addVideoIdInStorage(videoId){
+    chrome.storage.local.get(['videoList'], function(data){
+        if(data.videoList == undefined){
+            var array = []
+            chrome.storage.local.set({videoList: array})
+        }
+        data.videoList.push(videoId);
+        chrome.storage.local.set({videoList: data.videoList})
+    })
+}
