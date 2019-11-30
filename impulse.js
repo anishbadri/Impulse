4795af4fc48130e4a4320d854ca8cb1b627d8a5a
anishbadri@gmail.com <anishbadri@gmail.com>
2019-11-30 21:29:19 +0530

    delete with DOM

--- impulse.js
+++ impulse.js
@@ -38,46 +38,118 @@ chrome.storage.local.get(['videoList'], function(data){
     });
 })
 
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM loaded");
-    // document.getElementsByClassName(".delete-video").onclick = function() {
-    //     console.log("Clicked");
-    // }
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("DOM loaded");
+//     // document.getElementsByClassName(".delete-video").onclick = function() {
+//     //     console.log("Clicked");
+//     // }
 
-    document.querySelectorAll('button.delete-video').forEach(function(item){
-        // item.addEventListener('click', function(){
-        //     console.log(item);
-        // })
-        console.log(item);
-    })
-});
+//     // document.querySelectorAll('button.delete-video').forEach(function(item){
+//     //     // item.addEventListener('click', function(){
+//     //     //     console.log(item);
+//     //     // })
+//     //     console.log(item);
+//     // })
+// });
 
-window.setInterval(addButtonListener, 5000);
+window.setTimeout(addButtonListener, 5000);
 
 function addButtonListener(){
     document.querySelectorAll('button.delete-video').forEach(function(item){
         item.addEventListener('click', function(){
             console.log("clicked" + item.classList[0]);
-            removeVideo(item.classList[0]);
+            removeVideoFromStorage(item.classList[0]).then((result) => {
+                if (result){
+                    removeVideoDiv(item)
+                }
+            });
         })
+        // console.log(index + item);
     })
 }
 
-function removeVideo(videoId){
-    return new Promise((resolve, reject) =>{
+function removeVideoDiv(item){
+    console.log("removed");
+    var videoDiv = document.querySelector('#' + item.classList[0]);
+    videoDiv.parentNode.removeChild(videoDiv);
+}
+
+function removeVideoFromStorage(videoId){
+    return new Promise((resolve, reject) => { 
         chrome.storage.local.get(['videoList'], function(data){
             if(data.videoList != undefined){
-                data.videoList.forEach((index,element) => {
+                data.videoList.forEach((element, index) => {
+                    console.log(element.Id + "  " + videoId)
                     if(element.Id == videoId){
-                        // data.videoList.splice(index, 1);
-                        console.log(index);
+                        data.videoList.splice(index, 1);
+                        console.log(data.videoList);
+                        chrome.storage.local.set({videoList: data.videoList});
+                        resolve(true);
                     }
+                    // console.log(element.Id)
                 })
                 resolve(false);
             }
         });
     })
+    // console.log("removed");
 }
+
+const request = new Request('https://www.youtube.com/get_video_info?html5=1&video_id=' + '7mTGUzTgg08');
+
+fetch(request).then(response => {
+    console.log(response.status);
+});
+
+
+  
+// var request = require('request');
+
+// function qsToJson(qs) {
+//   var res = {};
+//   var pars = qs.split('&');
+//   var kv, k, v;
+//   for (i in pars) {
+//     kv = pars[i].split('=');
+//     k = kv[0];
+//     v = kv[1];
+//     res[k] = decodeURIComponent(v);
+//   }
+//   return res;
+// }
+
+// retrieve = function(id, callback) {
+//   var url = 'https://www.youtube.com/get_video_info?html5=1&video_id=' + id;
+  
+//   request(url, function(err, res, body) {
+//     if (!err && res.statusCode == 200) {
+//       var get_video_info = qsToJson(body);
+      
+//       // remapping urls into an array of objects
+//       var tmp = get_video_info["url_encoded_fmt_stream_map"];
+//       if (tmp) {
+//         tmp = tmp.split(',');
+//         for (i in tmp) {
+//           tmp[i] = qsToJson(tmp[i]);
+//         }
+//         get_video_info["url_encoded_fmt_stream_map"] = tmp;
+//       }
+      
+//       // done
+//       callback(null, get_video_info);
+//     }
+//     else {
+//       console.log('(youtube.get-video.info) HTTP response not 200/OK');
+//       callback(err, null);
+//     }
+//   });
+// }
+
+// var yt = require('youtube.get-video-info');
+// yt.retrieve('ml-v1bgMJDQ', function(err, res) {
+//   if (err) throw err;
+//   console.log(res);
+// });
 
 
 
